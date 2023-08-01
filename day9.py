827d5a0b8063cf48ca74c4c6998351f65b53f77b
jordicido <jordi.cidoncha4@gmail.com>
2023-08-01 07:21:28 +0200

    Day 9: Finished

--- day9.py
+++ day9.py
@@ -1,51 +1,50 @@
-       
+from itertools import permutations
+
+def calculate_total_distance(perm, paths):
+    distance = 0
+    for i in range(1, len(perm)):
+        distance += paths[(perm[i-1], perm[i])]
+
+    return distance
+
 def part1(input):
-    for i in range(len(input)):
-        input[i] = input[i].split(" = ")
-    
+
     locations = ['Faerun', 'Norrath', 'Tristram', 'AlphaCentauri', 'Arbre', 'Snowdin', 'Tambi', 'Straylight']
-    locations.sort()
-    possible_ways = []
-    cost = []
-    i = 7
+    all_permutations = permutations(locations)
+    paths = {}
+    for path in input:
+        cities = path.split(" = ")[0].split(" to ")
+        cost = int(path.split(" = ")[1])
+        paths[(cities[0], cities[1])] = cost
+        paths[(cities[1], cities[0])] = cost
 
-    while i > 0:
-        for j in range(0, i):
-            stop = input.pop(0)
-            stop_cost = int(stop[1])
-            stop = stop[0].split(" to ")
-            if i == 7:
-                possible_ways.append(stop)
-                cost.append(stop_cost)
-            else:
-                for x in range(0, len(possible_ways)):
-                    if possible_ways[x][-1] == stop[0]:
-                        possible_ways.append(possible_ways[x].copy())
-                        cost.append(cost[x])
-                        new_last_index = len(possible_ways) - 1
-                        possible_ways[new_last_index].append(stop[1])
-                        cost[new_last_index] += stop_cost
-                    elif possible_ways[x][-1] == stop[1]:
-                        possible_ways.append(possible_ways[x].copy())
-                        cost.append(cost[x])
-                        new_last_index = len(possible_ways) - 1
-                        possible_ways[new_last_index].append(stop[0])
-                        cost[new_last_index] += stop_cost
-        i -= 1
+    min_distance = float('inf')
 
-        
-    viable_costs = []
-    for i in range(len(possible_ways)):
-        possible_ways[i].sort()
-        if len(possible_ways[i]) == 8 and possible_ways[i] == locations:
-            viable_costs.append(cost[i])
-            print(possible_ways[i])
+    for perm in all_permutations:
+        distance = calculate_total_distance(perm, paths)
+        if distance < min_distance:
+            min_distance = distance
 
-    viable_costs.sort()
-    return viable_costs
-
+    return min_distance
+    
 def part2(input):
-    return
+    locations = ['Faerun', 'Norrath', 'Tristram', 'AlphaCentauri', 'Arbre', 'Snowdin', 'Tambi', 'Straylight']
+    all_permutations = permutations(locations)
+    paths = {}
+    for path in input:
+        cities = path.split(" = ")[0].split(" to ")
+        cost = int(path.split(" = ")[1])
+        paths[(cities[0], cities[1])] = cost
+        paths[(cities[1], cities[0])] = cost
+
+    max_distance = float('-inf')
+
+    for perm in all_permutations:
+        distance = calculate_total_distance(perm, paths)
+        if distance > max_distance:
+            max_distance = distance
+
+    return max_distance
 
 
 f = open("input/day9", "r")
